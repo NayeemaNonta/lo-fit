71093e3058dbe23a795c4a6a3c8516e4e6add3bc
NayeemaNonta <nnonta@uwaterloo.ca>
2025-09-12 10:50:55 -0400

    update trainer

--- utils/trainers.py
+++ utils/trainers.py
@@ -28,7 +28,7 @@ class CustomDPOTrainer(DPOTrainer):
     def get_batch_samples(self, *args, **kwargs):
         return [], 0
         
-    def compute_loss(self, model, inputs,return_outputs=False, num_items_in_batch: int = None):
+    def compute_loss(self, model, inputs,return_outputs=False):
         compute_loss_context_manager = torch.cuda.amp.autocast if self._peft_has_been_casted_to_bf16 else nullcontext
 
         with compute_loss_context_manager():
@@ -54,32 +54,93 @@ class CustomDPOTrainer(DPOTrainer):
         evaluate_tqa(eval_dataset,metrics=metrics,fname=fname,model=self.model,tokenizer=self.tokenizer,model_name=model_name)
         
         
-class CustomSFTTrainer(SFTTrainer):
-    def compute_loss(self, model, inputs,return_outputs=False):
-        labels = inputs['labels']
+# class CustomSFTTrainer(SFTTrainer):
+#     def compute_loss(self, model, inputs,return_outputs=False):
+#         labels = inputs['labels']
 
-        outputs = model(**inputs)
+#         outputs = model(**inputs)
         
-        ### Save past state if it exists
+#         ### Save past state if it exists
+#         if self.args.past_index >= 0:
+#             self._past = outputs[self.args.past_index]
+#         ### We don't use .loss here since the model may return tuples instead of ModelOutput.
+#         cn_loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+#         loss = cn_loss
+#         ### Add L1 regularization term
+#         l1norm = 0
+#         l1_lambda=self.l1_lambda
+        
+#         for param in model.parameters():
+#             if param.requires_grad:
+#                 l1norm+=param.abs().sum()
+#         loss+=l1_lambda*l1norm
+#         if return_outputs:
+#             return loss,outputs
+#         else:
+#             return loss
+       
+#     def test(self, fname,task,eval_dataset=None, ignore_keys=None, sanity_check=False,model_name=None,apply_chat_template=False,**kwargs):
+#         if sanity_check:
+#             print('Sanity check...')
+#         self.model.eval()
+#         self.args.prediction_loss_only = False
+#         self.tokenizer.add_eos_token = False
+#         if not os.path.exists(fname):
+#             os.makedirs(fname)
+#         if task == 'mquake':
+#             generated = evaluate_mquake(eval_dataset=eval_dataset,model_name=model_name,model=self.model,tokenizer=self.tokenizer,fname=fname,apply_chat_template=apply_chat_template)
+            
+#         elif task=='clutrr':
+#             generated = evaluate_clutrr(eval_dataset=eval_dataset,model_name=model_name,model=self.model,tokenizer=self.tokenizer,fname=fname,apply_chat_template=apply_chat_template)
+#         return generated
+
+class CustomSFTTrainer(SFTTrainer):
+    def compute_loss(
+        self,
+        model,
+        inputs,
+        return_outputs: bool = False,
+        num_items_in_batch: int = None,  # HF Trainer now passes this; we ignore it
+        **kwargs,                        # future-proof
+    ):
+        # forward pass
+        outputs = model(**inputs)
+
+        # Save past state if it exists (HF pattern)
         if self.args.past_index >= 0:
             self._past = outputs[self.args.past_index]
-        ### We don't use .loss here since the model may return tuples instead of ModelOutput.
-        cn_loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
-        loss = cn_loss
-        ### Add L1 regularization term
-        l1norm = 0
-        l1_lambda=self.l1_lambda
-        
-        for param in model.parameters():
-            if param.requires_grad:
-                l1norm+=param.abs().sum()
-        loss+=l1_lambda*l1norm
-        if return_outputs:
-            return loss,outputs
+
+        # Get loss regardless of output type
+        if hasattr(outputs, "loss"):
+            loss = outputs.loss
+        elif isinstance(outputs, dict) and "loss" in outputs:
+            loss = outputs["loss"]
         else:
-            return loss
-       
-    def test(self, fname,task,eval_dataset=None, ignore_keys=None, sanity_check=False,model_name=None,apply_chat_template=False,**kwargs):
+            # tuple case
+            loss = outputs[0]
+
+        # L1 regularization on trainable params
+        l1_lambda = getattr(self, "l1_lambda", 0.0)
+        if l1_lambda:
+            l1norm = 0.0
+            for p in model.parameters():
+                if p.requires_grad:
+                    l1norm += p.abs().sum()
+            loss = loss + l1_lambda * l1norm
+
+        return (loss, outputs) if return_outputs else loss
+
+    def test(
+        self,
+        fname,
+        task,
+        eval_dataset=None,
+        ignore_keys=None,
+        sanity_check=False,
+        model_name=None,
+        apply_chat_template=False,
+        **kwargs,
+    ):
         if sanity_check:
             print('Sanity check...')
         self.model.eval()
@@ -88,8 +149,21 @@ class CustomSFTTrainer(SFTTrainer):
         if not os.path.exists(fname):
             os.makedirs(fname)
         if task == 'mquake':
-            generated = evaluate_mquake(eval_dataset=eval_dataset,model_name=model_name,model=self.model,tokenizer=self.tokenizer,fname=fname,apply_chat_template=apply_chat_template)
-            
-        elif task=='clutrr':
-            generated = evaluate_clutrr(eval_dataset=eval_dataset,model_name=model_name,model=self.model,tokenizer=self.tokenizer,fname=fname,apply_chat_template=apply_chat_template)
+            generated = evaluate_mquake(
+                eval_dataset=eval_dataset,
+                model_name=model_name,
+                model=self.model,
+                tokenizer=self.tokenizer,
+                fname=fname,
+                apply_chat_template=apply_chat_template
+            )
+        elif task == 'clutrr':
+            generated = evaluate_clutrr(
+                eval_dataset=eval_dataset,
+                model_name=model_name,
+                model=self.model,
+                tokenizer=self.tokenizer,
+                fname=fname,
+                apply_chat_template=apply_chat_template
+            )
         return generated
