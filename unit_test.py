45c87539276b6f21de216fe9ce51f270192cb05e
NayeemaNonta <nnonta@uwaterloo.ca>
2025-09-23 05:05:34 -0400

    update past key val len

--- unit_test.py
+++ unit_test.py
@@ -2,7 +2,10 @@ from transformers import AutoTokenizer
 from models.modeling_llama import LlamaForCausalLM
 
 tok = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
-m = LlamaForCausalLM.custom_from_pretrained("meta-llama/Llama-3.2-1B")
+m = LlamaForCausalLM.custom_from_pretrained(
+    "meta-llama/Llama-3.2-1B",
+    cache_dir="/home/nnonta/.cache/huggingface"   # or another dir you want
+)
 
 x = tok("Hello, world!", return_tensors="pt")
 print(tok.decode(m.generate(**x, max_new_tokens=10)[0]))
